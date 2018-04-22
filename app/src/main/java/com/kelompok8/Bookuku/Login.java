dfb08958fb3aaa5202a146530bfd751457d1ef63
mafarienth <mugidya.mafarienth@gmail.com>
2018-04-22 23:54:59 +0700

    Tambah Intro Slider

--- app/src/main/java/com/kelompok8/Bookuku/Login.java
+++ app/src/main/java/com/kelompok8/Bookuku/Login.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -37,6 +38,21 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                startActivity(new Intent(Login.this, WelcomeActivity.class));
+                finish();
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
 
